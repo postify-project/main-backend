f40750505717324691b212118f7b9c3f1bf7a547
lestrofarhan <farhaneditztfg@gmail.com>
2026-07-23 23:21:57 +0500

    res pass resolved

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 
 
 
-// Middlewares
+// Middlewaresa
 app.use(cors({
   origin: [process.env.FRONTEND_URL || 'http://localhost:3000', process.env.REACT_URL || 'http://localhost:5173' ], // Frontend URL ko allow karne ke liye
   methods: ["POST", "GET", "PUT", "DELETE"],
